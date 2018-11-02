32ae8c86560d2502086f9b382ee03c3061995ee7
Andrii <andriy.bay@gmail.com>
2018-11-02 19:23:51 +0200

    test0211

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
+/*------------SELECT--------------------*/
+$(document).ready(function() {
+    $(".custom-select").each(function () {
+        var classes = $(this).attr("class"),
+            id = $(this).attr("id"),
+            name = $(this).attr("name"),
+            placeholder = $(this).attr("data-placeholder");
+        var template = '<span class="' + classes + '">';
+        template += '<span class="custom-select-trigger">' + placeholder + '</span>'
+        template += '<div class="custom-options">';
 
+        template += '<div class="scrollbar" id="scroll-style">';
+
+        $(this).find("option").each(function () {
+            template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+        });
+        template += '</div></div></div>';
+        $(this).wrap('<div class="custom-select-wrapper"></div>');
+        $(this).hide();
+        $(this).after(template);
+    });
+    $(".custom-option:first-of-type").hover(function () {
+        $(this).parents(".custom-options").addClass("option-hover");
+    }, function () {
+        $(this).parents(".custom-options").removeClass("option-hover");
+    });
+    $(".custom-select-trigger").on("click", function (e) {
+        $('html').one('click', function () {
+            $(".custom-select").removeClass("opened");
+        });
+        $(this).parents(".custom-select").toggleClass("opened");
+        //event.stopPropagation();
+        e.stopPropagation();
+
+    });
+    $(".custom-option").on("click", function () {
+        $(this).parents(".custom-select-wrapper").find("select").val($(this).data("value")).trigger('change');
+        $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
+        $(this).addClass("selection");
+        $(this).parents(".custom-select").removeClass("opened");
+        $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
+    });
+});
