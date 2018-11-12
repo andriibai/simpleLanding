1a69b8e8c23b36b5675ab555364ccaec8658b0f9
Andrii <andriy.bay@gmail.com>
2018-11-12 15:48:42 +0200

    test1211

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
+/*----------------SCROLLSPY----------------*/
+$(document).ready(function () {
+    const navbar = document.querySelector('#navbar');
+    const scrollspy = new VanillaScrollspy(navbar);
+    scrollspy.init();
+});
+/*-----------------------------------------*/
+/*------------HEDADER FIXED--------*/
+$(document).ready(function() {
+   /* var headerResize = $('#main-header').data('header-resize');
+    if (headerResize == 1) {
+        $(window).bind('scroll', toggleNavClass);
+    }
+    function toggleNavClass() {
+        var scrollTop = $(window).scrollTop();
+        var windowWidth = $(window).width();
+        $('#main-header').toggleClass('fixed', scrollTop > 124);
+    }*/
+});
+/*--------------MENU MOBILE-------*/
+$(document).ready(function() {
+    var $hederfixed = $('.header');
+    var $menu = $('.main-menu');
+    var $menulink = $('#nav-icon-mob');
+
+    $menulink.click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass('open');
+        $menu.toggleClass('active');
+        $hederfixed.addClass('fixed');
+    });
+
+    var $navlink = $('.main-menu ul li a');
+    $navlink.click(function (e) {
+        e.preventDefault();
+        $menu.removeClass('active');
+        $menulink.removeClass('open');
+        $hederfixed.removeClass('fixed');
+    });
+});
+/*--------------------------------*/
 /*------------SELECT--------------------*/
 $(document).ready(function() {
     $(".custom-select").each(function () {
@@ -58,13 +99,15 @@ $(document).ready(function() {
         slidesToScroll: 1,
         asNavFor: '.slider-nav',
         lazyLoad: 'ondemand',
+        //autoplay: true,
         speed: 1,
-        arrows: false,
         responsive: [
             {
                 breakpoint: 767,
                 settings: {
-                    arrows: true,
+                    prevArrow: $('.prev0'),
+                    nextArrow: $('.next0'),
+                    autoplay: true,
                     speed: 300,
                 }
             }
@@ -78,7 +121,7 @@ $(document).ready(function() {
         focusOnSelect: true,
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 767,
                 settings: {
                     slidesToShow: 1,
                     centerMode: false,
@@ -97,14 +140,14 @@ $(document).ready(function() {
 
     $('.experts-slider').slick({
         dots:false,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'),
+        prevArrow: $('.prev1'),
+        nextArrow: $('.next1'),
         infinite: true,
         cssEase: 'linear',
         speed: 300,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3000,
         responsive: [
             {
@@ -139,39 +182,16 @@ $(document).ready(function() {
     });
     $('.reviews-slider').slick({
         dots:true,
-        centerMode:true,
         prevArrow: $('.prev2'),
         nextArrow: $('.next2'),
         infinite: true,
         cssEase: 'linear',
+        adaptiveHeight: true,
         speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3000,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
     });
 
 
@@ -198,7 +218,7 @@ $(document).ready(function() {
         speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3000
     });
 
